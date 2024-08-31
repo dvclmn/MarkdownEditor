fcdb890c5693a925e91671c8596caf05a0dd54fb
Dave Coleman <dave@dvclmn.com>
2024-08-31 11:05:42 +0200

    Removes a lot of older code

--- Sources/MarkdownEditor/Other stuff/System Extensions/Ranges.swift
+++ Sources/MarkdownEditor/Other stuff/System Extensions/Ranges.swift
@@ -11,7 +11,9 @@ import AppKit
 
 
 
-
+/// Having this here (at least for now) seems to avoid conflicts between `STTextKitPlus`
+/// and `Rearrange`.
+///
 public extension Range where Bound == String.Index {
   
   func textRange(in string: String, provider: NSTextElementProvider) -> NSTextRange? {
@@ -23,9 +25,6 @@ public extension Range where Bound == String.Index {
     }
     
     let documentLocation: NSTextLocation = provider.documentRange.location
-    
-//    let oldStart: Int = self.lowerBound.utf16Offset(in: string)
-//    let oldEnd: Int = self.upperBound.utf16Offset(in: string)
     
     // Use a safer method to get UTF-16 offsets
     let oldStart: Int = string.distance(from: string.startIndex, to: self.lowerBound)
@@ -48,81 +47,3 @@ public extension Range where Bound == String.Index {
     
   }
 }
-
-
-
-//
-//extension NSTextContentManager {
-//  func range(for textRange: NSTextRange) -> NSRange? {
-//    let location = offset(from: documentRange.location, to: textRange.location)
-//    let length = offset(from: textRange.location, to: textRange.endLocation)
-//    if location == NSNotFound || length == NSNotFound { return nil }
-//    return NSRange(location: location, length: length)
-//  }
-//  
-//  func textRange(for range: NSRange) -> NSTextRange? {
-//    guard let textRangeLocation = location(documentRange.location, offsetBy: range.location),
-//          let endLocation = location(textRangeLocation, offsetBy: range.length) else { return nil }
-//    return NSTextRange(location: textRangeLocation, end: endLocation)
-//  }
-//}
-//
-//extension MarkdownTextView {
-//  
-//  public func textRange(for rect: NSRect) -> NSRange {
-//    let length = self.textStorage?.length ?? 0
-//    
-//    guard let layoutManager = self.layoutManager else {
-//      return NSRange(0..<length)
-//    }
-//    
-//    guard let container = self.textContainer else {
-//      return NSRange(0..<length)
-//    }
-//    
-//    let origin = textContainerOrigin
-//    let offsetRect = rect.offsetBy(dx: origin.x, dy: origin.y)
-//    
-//    let glyphRange = layoutManager.glyphRange(forBoundingRect: offsetRect, in: container)
-//    
-//    return layoutManager.characterRange(forGlyphRange: glyphRange, actualGlyphRange: nil)
-//  }
-//  
-//  var visibleTextRange: NSRange {
-//    return textRange(for: visibleRect)
-//  }
-//
-//  /// All the selected ranges, as NSRange instances
-//  public var selectedTextRanges: [NSRange] {
-//    get {
-//      return selectedRanges.map({ $0.rangeValue })
-//    }
-//    set {
-//      selectedRanges = newValue.map { NSValue(range: $0) }
-//    }
-//  }
-//
-//  /// A single range representing a single, continuous selection
-//  ///
-//  /// This method returns nil if there isn't exactly one selection range.
-//  public var selectedContinuousRange: NSRange? {
-//    let ranges = selectedTextRanges
-//    if ranges.count != 1 {
-//      return nil
-//    }
-//    
-//    return ranges.first!
-//  }
-//
-//  /// A singlel location representing a single, zero-length selection
-//  ///
-//  /// This method returns nil if there is more than one selected range,
-//  /// or if that range has a non-zero length.
-//  public var insertionLocation: Int? {
-//    guard let range = selectedContinuousRange else {
-//      return nil
-//    }
-//    
-//    return range.length == 0 ? range.location : nil
-//    }
-//}
