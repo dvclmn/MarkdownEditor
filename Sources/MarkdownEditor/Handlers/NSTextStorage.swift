700de92ba4680bd7ab7a50e07428d48798c618bc
Dave Coleman <dave@dvclmn.com>
2025-02-10 09:24:20 +0100

    Improvements to text storage styling

--- Sources/MarkdownEditor/Handlers/NSTextStorage.swift
+++ Sources/MarkdownEditor/Handlers/NSTextStorage.swift
@@ -105,49 +105,85 @@ class MarkdownTextStorage: NSTextStorage {
   }
 
   private func highlightCodeBlocks() {
-
     self.beginEditing()
     guard let regex = Markdown.Syntax.codeBlock.nsRegex else { return }
     let text = backingStore.string
     let range = NSRange(location: 0, length: backingStore.length)
-
+    
     regex.enumerateMatches(in: text, options: [], range: range) { match, _, _ in
       guard let match = match else { return }
-
-      // Ensure the match range is valid
+      
       if match.range.location + match.range.length <= backingStore.length {
-
-        /// Extract the code content (without backticks and language hint)
+        let fullRange = match.range
         let codeBlock = (text as NSString).substring(with: fullRange)
         let lines = codeBlock.components(separatedBy: .newlines)
-
-        /// Extract language hint from the first line
+        
         let languageHint = lines.first?
           .replacingOccurrences(of: "```", with: "")
           .trimmingCharacters(in: .whitespaces)
-
-        /// Highlight the code
-        guard let highlightr else { return }
-//        highlightr.setTheme(to: "devibeans")
-        guard let highlightedCode = highlightr.highlight(codeBlock, as: languageHint ?? "txt")
-        else {
+        
+        guard let highlightr = highlightr,
+              let highlightedCode = highlightr.highlight(codeBlock, as: languageHint ?? "txt") else {
           return
         }
-        /// Create attributed string with the highlighted code
+        
         let attributedCode = NSMutableAttributedString(attributedString: highlightedCode)
-
-        /// Add the code block background attribute to the entire range
         attributedCode.addAttribute(
           TextBackground.codeBlock.attributeKey,
           value: true,
-          range: NSRange(location: 0, length: attributedCode.length))
-
-        /// Replace the content while preserving the backticks
+          range: NSRange(location: 0, length: attributedCode.length)
+        )
+        
         backingStore.replaceCharacters(in: fullRange, with: attributedCode)
       }
     }
     self.endEditing()
   }
+  
+//  private func highlightCodeBlocks() {
+//
+//    self.beginEditing()
+//    guard let regex = Markdown.Syntax.codeBlock.nsRegex else { return }
+//    let text = backingStore.string
+//    let range = NSRange(location: 0, length: backingStore.length)
+//
+//    regex.enumerateMatches(in: text, options: [], range: range) { match, _, _ in
+//      guard let match = match else { return }
+//
+//      // Ensure the match range is valid
+//      if match.range.location + match.range.length <= backingStore.length {
+//
+//        /// Extract the code content (without backticks and language hint)
+//        let codeBlock = (text as NSString).substring(with: fullRange)
+//        let lines = codeBlock.components(separatedBy: .newlines)
+//
+//        /// Extract language hint from the first line
+//        let languageHint = lines.first?
+//          .replacingOccurrences(of: "```", with: "")
+//          .trimmingCharacters(in: .whitespaces)
+//
+//        /// Highlight the code
+//        guard let highlightr else { return }
+////        highlightr.setTheme(to: "devibeans")
+//        guard let highlightedCode = highlightr.highlight(codeBlock, as: languageHint ?? "txt")
+//        else {
+//          return
+//        }
+//        /// Create attributed string with the highlighted code
+//        let attributedCode = NSMutableAttributedString(attributedString: highlightedCode)
+//
+//        /// Add the code block background attribute to the entire range
+//        attributedCode.addAttribute(
+//          TextBackground.codeBlock.attributeKey,
+//          value: true,
+//          range: NSRange(location: 0, length: attributedCode.length))
+//
+//        /// Replace the content while preserving the backticks
+//        backingStore.replaceCharacters(in: fullRange, with: attributedCode)
+//      }
+//    }
+//    self.endEditing()
+//  }
 
   private func processRegexMatches(
     for syntax: Markdown.Syntax,
@@ -156,43 +192,121 @@ class MarkdownTextStorage: NSTextStorage {
     applyAttributes: (MarkdownRanges) -> Void
   ) {
     let range = NSRange(location: 0, length: backingStore.length)
-
-    pattern.enumerateMatches(in: text, options: [], range: range) {
-      match, _, _ in
+    
+    pattern.enumerateMatches(in: text, options: [], range: range) { match, _, _ in
       guard let match = match else { return }
-
+      
       /// Ensure the match range is valid
       if match.range.location + match.range.length <= backingStore.length {
-        if match.numberOfRanges == 4 {
-          /// Extract capture groups
-          let openingSyntaxRange = match.range(at: 1)
-          let contentRange = match.range(at: 2)
-          let closingSyntaxRange = match.range(at: 3)
-
-          /// Apply attributes using the provided closure
-          let ranges = MarkdownRanges(
-            all: match.range,
-            leading: openingSyntaxRange,
-            content: contentRange,
-            trailing: closingSyntaxRange
-          )
-          applyAttributes(ranges)
-        } else {
-          fatalError("Figure this out")
-          let ranges = MarkdownRanges(
-            all: match.range,
-            leading: .zero,
-            content: .zero,
-            trailing: .zero
-          )
-          /// Fallback for simpler patterns
-          applyAttributes(ranges)
+        switch syntax {
+          case .bold, .italic, .boldItalic, .strikethrough, .inlineCode, .highlight:
+            if match.numberOfRanges == 4 {
+              let leadingRange = match.range(at: 1)
+              let contentRange = match.range(at: 2)
+              let trailingRange = match.range(at: 3)
+              
+              let ranges = MarkdownRanges(
+                all: match.range,
+                leading: leadingRange,
+                content: contentRange,
+                trailing: trailingRange
+              )
+              applyAttributes(ranges)
+            }
+          case .heading, .list, .quoteBlock:
+            if match.numberOfRanges == 3 {
+              let leadingRange = match.range(at: 1)
+              let contentRange = match.range(at: 2)
+              
+              let ranges = MarkdownRanges(
+                all: match.range,
+                leading: leadingRange,
+                content: contentRange,
+                trailing: .zero
+              )
+              applyAttributes(ranges)
+            }
+          case .link, .image:
+            if match.numberOfRanges == 6 {
+              let leadingRange = match.range(at: 1)
+              let contentRange = match.range(at: 2)
+              let urlRange = match.range(at: 4)
+              
+              let ranges = MarkdownRanges(
+                all: match.range,
+                leading: leadingRange,
+                content: contentRange,
+                trailing: urlRange
+              )
+              applyAttributes(ranges)
+            }
+          case .codeBlock:
+            if match.numberOfRanges == 4 {
+              let leadingRange = match.range(at: 1)
+              let contentRange = match.range(at: 2)
+              let trailingRange = match.range(at: 3)
+              
+              let ranges = MarkdownRanges(
+                all: match.range,
+                leading: leadingRange,
+                content: contentRange,
+                trailing: trailingRange
+              )
+              applyAttributes(ranges)
+            }
+          default:
+            break
         }
       } else {
         print("Invalid range: \(match.range) for string length: \(backingStore.length)")
       }
     }
   }
+  
+//  private func processRegexMatches(
+//    for syntax: Markdown.Syntax,
+//    in text: String,
+//    using pattern: NSRegularExpression,
+//    applyAttributes: (MarkdownRanges) -> Void
+//  ) {
+//    let range = NSRange(location: 0, length: backingStore.length)
+//
+//    pattern.enumerateMatches(in: text, options: [], range: range) {
+//      match, _, _ in
+//      guard let match = match else { return }
+//
+//      /// Ensure the match range is valid
+//      if match.range.location + match.range.length <= backingStore.length {
+//        if match.numberOfRanges == 4 {
+//          /// Extract capture groups
+//          let openingSyntaxRange = match.range(at: 1)
+//          let contentRange = match.range(at: 2)
+//          let closingSyntaxRange = match.range(at: 3)
+//
+//          /// Apply attributes using the provided closure
+//          let ranges = MarkdownRanges(
+//            all: match.range,
+//            leading: openingSyntaxRange,
+//            content: contentRange,
+//            trailing: closingSyntaxRange
+//          )
+//          applyAttributes(ranges)
+//        } else {
+//          fatalError("Figure this out")
+//          let ranges = MarkdownRanges(
+//            all: match.range,
+//            leading: .zero,
+//            content: .zero,
+//            trailing: .zero
+//          )
+//          /// Fallback for simpler patterns
+//          applyAttributes(ranges)
+//        }
+//      } else {
+//        print("Invalid range: \(match.range) for string length: \(backingStore.length)")
+//      }
+//    }
+//  }
 
 
 }
